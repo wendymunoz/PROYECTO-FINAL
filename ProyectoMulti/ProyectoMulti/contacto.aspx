ee062d1023d5ac447c773f11075df624a31e43e5
WENDY <wendy.munoz@bqm.com.pe>
2017-09-25 17:33:03 -0500

    contacto

---
+++ ProyectoMulti/ProyectoMulti/contacto.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PagMaestra.Master" AutoEventWireup="true" CodeBehind="contacto.aspx.cs" Inherits="ProyectoMulti.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+    Contáctenos</p>
+<p>
+    Nombres:
+    <asp:TextBox ID="NombreTxt" runat="server"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Completar este campo" CssClass="alerta" Display="Dynamic"></asp:RequiredFieldValidator>
+</p>
+<p>
+    Apellidos:
+    <asp:TextBox ID="ApellidosTxt" runat="server"></asp:TextBox>
+</p>
+<p>
+    Correo:
+    <asp:TextBox ID="CorreoTxt" runat="server" OnTextChanged="TextBox4_TextChanged"></asp:TextBox>
+</p>
+<p>
+    Mensaje:</p>
+<p>
+    <asp:TextBox ID="MensajeTxt" runat="server" Height="74px" Width="180px" OnTextChanged="TextBox3_TextChanged"></asp:TextBox>
+</p>
+<p>
+    <asp:Button ID="Enviarutin" runat="server" Text="Button" />
+</p>
+</asp:Content>
+
+
---
+++ ProyectoMulti/ProyectoMulti/contacto.aspx.designer.cs
@@ -0,0 +1,69 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ProyectoMulti {
+    
+    
+    public partial class WebForm1 {
+        
+        /// <summary>
+        /// Control NombreTxt.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox NombreTxt;
+        
+        /// <summary>
+        /// Control RequiredFieldValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+        
+        /// <summary>
+        /// Control ApellidosTxt.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox ApellidosTxt;
+        
+        /// <summary>
+        /// Control CorreoTxt.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox CorreoTxt;
+        
+        /// <summary>
+        /// Control MensajeTxt.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MensajeTxt;
+        
+        /// <summary>
+        /// Control Enviarutin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Enviarutin;
+    }
+}
