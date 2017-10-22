d636e6b5eb8cf0b0adaa49300c5e313448109f9a
gperezusil <gpch9413@gmail.com>
2017-10-22 16:28:05 -0500

    PaginaMaestra
    Estrucutura de pagina maestra

--- ProyectoMulti/ProyectoMulti/PagMaestra.Master.designer.cs
+++ ProyectoMulti/ProyectoMulti/PagMaestra.Master.designer.cs
@@ -38,5 +38,23 @@ namespace ProyectoMulti {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// Control txtCorreo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCorreo;
+        
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
