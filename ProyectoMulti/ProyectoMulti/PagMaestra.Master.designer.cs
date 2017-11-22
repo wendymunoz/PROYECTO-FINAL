6d4aff06436acf18cd600e608b081cb766370fe5
wendymunoz <wendy.munoz@bqm.com.pe>
2017-11-22 14:18:30 -0500

    login master

--- ProyectoMulti/ProyectoMulti/PagMaestra.Master.designer.cs
+++ ProyectoMulti/ProyectoMulti/PagMaestra.Master.designer.cs
@@ -22,6 +22,15 @@ namespace ProyectoMulti {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Control LoginView1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginView LoginView1;
+        
+        /// <summary>
         /// Control Repeater1.
         /// </summary>
         /// <remarks>
