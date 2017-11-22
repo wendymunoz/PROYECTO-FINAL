f664c0a2a36b644cfd0b55ae55694ecb787ab244
WENDY <wendy.munoz@bqm.com.pe>
2017-11-21 23:31:57 -0500

    nuevo login

--- ProyectoMulti/ProyectoMulti/login.aspx.cs
+++ ProyectoMulti/ProyectoMulti/login.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+
 
 namespace ProyectoMulti
 {
@@ -13,5 +15,31 @@ namespace ProyectoMulti
         {
 
         }
+
+        protected void OnClick1_Button(object sender, EventArgs e)
+        {
+
+            int numero;
+            numero = 0;
+            DataView dvSql = (DataView)SqlDataSource2.Select(DataSourceSelectArguments.Empty);
+
+            if (dvSql.Count > 0)
+            {
+                numero = 1;
+            }
+            else
+            {
+                Response.Redirect("login.aspx");
+            }
+            if (numero == 1)
+            {
+                //Session["usuario"] = dvSql[0].DataView[0];
+                Session["Usuarios"] = dvSql[0].Row[0];
+                Response.Redirect("Default.aspx");
+                
+            }
+
+        }
+
     }
 }
--- ProyectoMulti/ProyectoMulti/login.aspx.designer.cs
+++ ProyectoMulti/ProyectoMulti/login.aspx.designer.cs
@@ -13,12 +13,66 @@ namespace ProyectoMulti {
     public partial class login {
         
         /// <summary>
-        /// Login1 control.
+        /// Label1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Login Login1;
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// EmailTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox EmailTextBox;
+        
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// ContraTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox ContraTextBox;
+        
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// SqlDataSource2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
     }
 }
